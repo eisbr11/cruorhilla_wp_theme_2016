a31133605d166018dbf3ec8033af7f40ad5a9295
Felix Schliemann <felix.schliemann@netformic.de>
2018-05-22 15:34:40 +0200

    new redesign for new album release

--- index.php
+++ index.php
@@ -8,51 +8,62 @@
  *
  * Methods for TimberHelper can be found in the /lib sub-directory
  *
- * @package  WordPress
+ * @package     WordPress
  * @subpackage  Timber
- * @since   Timber 0.1
+ * @since       Timber 0.1
  */
 
-if ( ! class_exists( 'Timber' ) ) {
-	echo 'Timber not activated. Make sure you activate the plugin in <a href="/wp-admin/plugins.php#timber">/wp-admin/plugins.php</a>';
-	return;
+if (!class_exists('Timber')) {
+    echo 'Timber not activated. Make sure you activate the plugin in <a href="/wp-admin/plugins.php#timber">/wp-admin/plugins.php</a>';
+    return;
 }
 
 $context = Timber::get_context();
 
 //$context['posts'] = Timber::get_posts();
 
-$args = array(
-	'post_type'    => 'concert',
-	'orderby'     => 'meta_value',
-	'order'        => 'ASC',
+$args = [
+    'post_type' => 'concert',
+    'orderby'   => 'meta_value',
+    'order'     => 'ASC',
 
-	'meta_key'     => 'gig_date',
-	'meta_value'   => date( "Ymd" ), // change to how "event date" is stored
-	'meta_compare' => '>=',
-);
+    'meta_key'     => 'gig_date',
+    'meta_value'   => date("Ymd"), // change to how "event date" is stored
+    'meta_compare' => '>=',
+];
 $context['concerts'] = Timber::get_posts($args);
 
-$args = array(
-	'post_type' => 'video',
-	'orderby' => 'meta_value_num',
-	'meta_key' => 'position',
-);
-
+$args = [
+    'post_type' => 'video',
+    'orderby'   => 'meta_value_num',
+    'meta_key'  => 'position',
+];
 $context['teaser_vids'] = Timber::get_posts($args);
 
-$args = array(
-	'post_type' => 'releases',
-);
+$album_args = [
+    'post_type'    => 'releases',
+    'orderby'      => 'meta_value',
+    'order'        => 'DESC',
+    'meta_key'     => 'release_date',
+    'offset'       => 1,
+];
+$context['albums'] = Timber::get_posts($album_args);
 
+
+$args = [
+    'post_type'    => 'releases',
+    'orderby'      => 'release_date',
+    'order'        => 'DESC',
+    'meta_key'     => 'release_date',
+];
 $context['teaser_release'] = Timber::get_post($args);
 
 $past_gigs_id = 20;
 
 $context['past_gigs'] = Timber::get_post($past_gigs_id);
 
-$templates = array( 'index.twig' );
-if ( is_home() ) {
-	array_unshift( $templates, 'home.twig' );
+$templates = ['index.twig'];
+if (is_home()) {
+    array_unshift($templates, 'home.twig');
 }
-Timber::render( $templates, $context );
+Timber::render($templates, $context);
